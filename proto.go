f1ae922492c292da3e97f1aa75d43f4e8d2c3ae5
Michael Johnson <michael@johnson.computer>
2015-07-18 16:55:37 -0400

    Add code for converting between UUID types.

--- proto.go
+++ proto.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"encoding/binary"
+
 	"code.google.com/p/go-uuid/uuid"
+	"github.com/golang/protobuf/proto"
 
 	messages "github.com/foxelbox/foxbukkitslacklink/messages"
 )
 
 //go:generate protoc --go_out=messages messages.proto
 
-// protoToUUID converts a protobuf UUID (of two 64-bit integers) to a UUID that
-// we can use.
-func protoToUUID(m messages.UUID) uuid.UUID {
-	return nil
+func protoUUIDToUUID(u *messages.UUID) uuid.UUID {
+	out := make(uuid.UUID, 16)
+
+	binary.BigEndian.PutUint64(out, uint64(*u.Msb))
+	binary.BigEndian.PutUint64(out[8:], uint64(*u.Lsb))
+
+	return out
+}
+
+func uuidToProtoUUID(u uuid.UUID) *messages.UUID {
+	return &messages.UUID{
+		Msb: proto.Int64(int64(binary.BigEndian.Uint64(u[:8]))),
+		Lsb: proto.Int64(int64(binary.BigEndian.Uint64(u[8:]))),
+	}
 }
